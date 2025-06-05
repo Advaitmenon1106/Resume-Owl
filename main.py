73d216175b9994a3c5ba7a9f5e76c6c0377d64aa
advaitmenon1106 <advaitmenon1106@gmail.com>
2025-06-05 23:34:12 +0530

    Run and test the agent in a main file.

---
+++ main.py
@@ -0,0 +1,8 @@
+from agents.main_agent import graph_builder, state_initialiser
+import asyncio
+
+state = state_initialiser('sample_inputs/job_descriptions.csv', 'sample_inputs/Advait-Menon_May_2025_Resume.docx')
+agent = graph_builder()
+res = asyncio.run(agent.ainvoke(state))
+print(res['relevant_job_fields'])
+print(res['mapped_resume_fields_to_job_fields'])
