c11a44892c5d46d3194b9cf0df454000f81c55c8
advaitmenon1106 <advaitmenon1106@gmail.com>
2025-06-07 23:58:52 +0530

    Refactor imports after the file name. Also add a print statement for a visual checkpoint of progress.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from agents.main_agent import graph_builder, state_initialiser
+from agents.preprocessing_agent import graph_builder, state_initialiser
 import asyncio
 
 state = state_initialiser('sample_inputs/job_descriptions.csv', 'sample_inputs/Advait-Menon_May_2025_Resume.docx')
 agent = graph_builder()
+print("Running the preprocessing agent")
 res = asyncio.run(agent.ainvoke(state))
 print(res['relevant_job_fields'])
 print(res['mapped_resume_fields_to_job_fields'])
