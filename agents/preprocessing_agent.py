a9f596ea13a11d4e7ecd821cc6b1a8203098edb7
advaitmenon1106 <advaitmenon1106@gmail.com>
2025-06-13 00:22:40 +0530

    Clean up the code. Use a mix of Gemini and Mistral to avoid crossing rate limits.

--- agents/preprocessing_agent.py
+++ agents/preprocessing_agent.py
@@ -1,11 +1,12 @@
 import asyncio
-from typing_extensions import TypedDict, Any
+from typing_extensions import TypedDict
 from langgraph.graph import StateGraph
 from input_preprocessing.input_preprocessing import convert_csv_to_json, convert_resume_to_json
-from mistralai import Mistral
-import os
 from dotenv import load_dotenv
 import yaml
+import json
+from utils import prompt_gemini, prompt_mistral
+
 
 load_dotenv()
 
@@ -13,18 +14,6 @@ load_dotenv()
 with open('prompts.yml') as f:
     prompts = yaml.safe_load(f)
     prompts = prompts['preprocessing_agent']
-
-
-async def prompt_mistral(prompt_text):
-    client = Mistral(api_key=os.environ['MISTRAL_API_KEY'])
-    model = "mistral-large-latest"
-    messages = [{"role": "user", "content": prompt_text}]
-    
-    def sync_call():
-        return client.chat.complete(model=model, messages=messages, temperature=0)
-    
-    response = await asyncio.to_thread(sync_call)
-    return response.choices[0].message.content
 
 
 class AgentState(TypedDict):
@@ -44,10 +33,11 @@ def state_initialiser(fp_data, fp_resume):
 
 
 async def relevant_job_field_extractor(state:AgentState):
+    await asyncio.sleep(1.0)
     single_job_req = state['jobs_json'][0]
     input_prompt = prompts['relevant_job_field_extractor'].format(single_job_req=single_job_req)
 
-    relevant_job_fields_cs = await prompt_mistral(input_prompt)
+    relevant_job_fields_cs = await prompt_gemini(input_prompt)
     relevant_job_fields = relevant_job_fields_cs.strip().split(',')
     relevant_job_fields = [i.strip() for i in relevant_job_fields]
     state['relevant_job_fields'] = relevant_job_fields
@@ -65,16 +55,19 @@ async def resumeKeys_to_jobKeys_mapper(state:AgentState):
 
     mapped_resume_fields_to_job_fields = {}
 
-    for j_key in job_keys:
-        imp_fields_cs = await prompt_mistral(input_prompt.format(j_key=j_key, resume_keys=resume_keys))
-        imp_fields = imp_fields_cs.strip().split(',')
-        imp_fields = [i.strip() for i in imp_fields]
+    # for j_key in job_keys:
+    #     await asyncio.sleep(2.0)
+    #     imp_fields_cs = await prompt_mistral(input_prompt.format(j_key=j_key, resume_keys=resume_keys))
+    #     imp_fields = imp_fields_cs.strip().split(',')
+    #     imp_fields = [i.strip() for i in imp_fields]
 
-        mapped_resume_fields_to_job_fields[j_key] = imp_fields
+    #     mapped_resume_fields_to_job_fields[j_key] = imp_fields
+
+    json_mappings = await prompt_mistral(prompt_text=input_prompt.format(job_keys=job_keys, resume_keys=resume_keys))
+    mapped_resume_fields_to_job_fields = json.dumps(json_mappings)
     
     state['mapped_resume_fields_to_job_fields'] = mapped_resume_fields_to_job_fields
     return state
-
 
 
 def graph_builder():
